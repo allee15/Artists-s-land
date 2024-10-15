95be4defbfeb9904aec4ed621971051f6f98abdf
Alexia Aldea <alexia.elena.aldea@gmail.com>
2024-10-15 22:00:14 +0300

    first comit

---
+++ MobileApp/ArtistsLand/ArtistsLand/ArtistsLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistsLandApp.swift
+//  ArtistsLand
+//
+//  Created by Alexia Aldea on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ArtistsLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
