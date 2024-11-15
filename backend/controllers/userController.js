73496072723e28a55884f2237963c8285f150c3f
Cristea Bianca <bianca.cristea01@yahoo.com>
2024-11-15 19:03:23 +0200

    Add change password functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const editAccount = async (req, res) => {
   try {
@@ -11,7 +12,46 @@ export const editAccount = async (req, res) => {
     await user.save();
     res.status(200).json({ message: "The account was edited succesfully" });
   } catch (error) {
-    console.log("Error in editAccount function", error.message);
+    console.log("Error in editAccount controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword, confirmNewPassword } = req.body;
+
+    if (!currentPassword || !newPassword || !confirmNewPassword) {
+      return res.status(400).json({
+        message:
+          "Please provide current password, new password and confirm new password",
+      });
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      return res.status(400).json({
+        message: "New Password and Confirm New Password should be the same",
+      });
+    }
+
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "You introduced the wrong current password" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).json({ message: "The password was changed succesfully" });
+  } catch (error) {
+    console.log("Error in change password controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import authorize from "../middleware/authorization.js";
-import { editAccount } from "../controllers/userController.js";
+import { changePassword, editAccount } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.put("/edit-acc", authorize, editAccount);
+router.put("/change-password", authorize, changePassword);
 
 export default router;
