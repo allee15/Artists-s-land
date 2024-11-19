fc33f335f75e99c4d1c941942ad0fa786110978e
Cristea Bianca <bianca.cristea01@yahoo.com>
2024-11-19 18:43:59 +0200

    Add functionality to get all messages from a conversation

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/messageController.js";
+import { sendMessage, getMessages } from "../controllers/messageController.js";
 import authorize from "../middleware/authorization.js";
 
 const router = express.Router();
 
+router.get("/receive/:id", authorize, getMessages);
 router.post("/send/:id", authorize, sendMessage);
 
 export default router;
